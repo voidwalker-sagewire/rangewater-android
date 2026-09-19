df88057ca065d6e50d3137b998245148bcfaebbf
Mike Miller <voidwalker@sagewiresyndicate.com>
2026-09-19 12:29:32 -0400

    Fix Layers button compile import

--- app/src/main/java/com/sagewire/rangewater/ui/map/MapScreen.kt
+++ app/src/main/java/com/sagewire/rangewater/ui/map/MapScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
